f9ef744732b7d159feb696d4bdd9b8f442bc153b
Krzysztof Pniak <krzysztof.pniak@cardinality.co.uk>
2018-10-07 12:06:15 +0200

    chore(babel+rollup): switch to babel 7 and setup jest

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-//import resolve from 'rollup-plugin-node-resolve';
+import nodeResolve from 'rollup-plugin-node-resolve';
 //import commonjs from 'rollup-plugin-commonjs';
 //import globals from 'rollup-plugin-node-globals';
 //import builtins from 'rollup-plugin-node-builtins';
@@ -7,33 +7,29 @@ import pkg from './package.json';
 
 export default [
 	// browser-friendly UMD build
-	/*{
-		entry: 'src/main.js',
-		dest: pkg.browser,
-		format: 'umd',
-		moduleName: 'k-reducer',
+	{
+		input: 'src/main.js',
+		output: {file: pkg.browser, format: 'umd', name: 'k-reducer'},
 		plugins: [
-            globals(),
-            builtins(),
-			resolve(), // so Rollup can find `ms`
-			commonjs(), // so Rollup can convert `ms` to an ES module
+            nodeResolve({
+                jsnext: true
+            }),
 			babel({
 				exclude: ['node_modules/**']
 			})
 		]
-	},*/
-
+	},
 	// CommonJS (for Node) and ES module (for bundlers) build.
 	// (We could have three entries in the configuration array
 	// instead of two, but it's quicker to generate multiple
 	// builds from a single configuration where possible, using
 	// the `targets` option which can specify `dest` and `format`)
 	{
-		entry: 'src/main.js',
+		input: 'src/main.js',
 		external: ['ramda'],
-		targets: [
-			{ dest: pkg.main, format: 'cjs' },
-			{ dest: pkg.module, format: 'es' }
+		output: [
+			{ file: pkg.main, format: 'cjs' },
+			{ file: pkg.module, format: 'es' }
 		],
 		plugins: [
 			babel({
