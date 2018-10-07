c7caaf3e2d59e40769e9a268d5521c5d89477b88
Krzysztof Pniak <krzysztof.pniak@cardinality.co.uk>
2018-10-07 14:37:12 +0200

    docs(jsdoc): test

--- src/main.js
+++ src/main.js
@@ -21,6 +21,31 @@ const actionType2 = (type, transform) => (state, action) => propEq('type', type,
 
 const action = (matcher, transform) => (state, action) => matcher(action) ? uncurryN(2, transform)(action.payload, state) : state;
 
+/**
+ * Applies a function to the value at the given index of an array, returning a
+ * new copy of the array with the element at the given index replaced with the
+ * result of the function application.
+ *
+ * @func
+ * @memberOf R
+ * @since v0.14.0
+ * @category List
+ * @sig Number -> (a -> a) -> [a] -> [a]
+ * @param {Number} idx The index.
+ * @param {Function} fn The function to apply.
+ * @param {Array|Arguments} list An array-like object whose value
+ *        at the supplied index will be replaced.
+ * @return {Array} A copy of the supplied array-like object with
+ *         the element at index `idx` replaced with the value
+ *         returned by applying `fn` to the existing element.
+ * @see R.update
+ * @example
+ *
+ *      R.adjust(1, R.toUpper, ['a', 'b', 'c', 'd']);      //=> ['a', 'B', 'c', 'd']
+ *      R.adjust(-1, R.toUpper, ['a', 'b', 'c', 'd']);     //=> ['a', 'b', 'c', 'D']
+ * @symb R.adjust(-1, f, [a, b]) = [a, f(b)]
+ * @symb R.adjust(0, f, [a, b]) = [f(a), b]
+ */
 const root = (type, subReducer) => (state, action) => {
     const prefix = `${type}.`;
     if (compose(startsWith(prefix), prop('type'))(action)) {
