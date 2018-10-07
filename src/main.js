dd59fe7e302715f05035681882e57c22516c345a
Krzysztof Pniak <krzysztof.pniak@cardinality.co.uk>
2018-10-07 23:40:49 +0200

    refactor(wrapAction): more clear filter

--- src/main.js
+++ src/main.js
@@ -41,7 +41,7 @@ const wrapAction = (action, ...types) => {
     if (types.length === 0) {
         return action;
     } else {
-        if (types.some(type => ~type.toString().indexOf('.'))) { // eslint-disable-line
+        if (types.some(type => type.indexOf('.') !== -1)) { // eslint-disable-line
             throw new Error('Action type can\'t contain a dot');
         }
 
