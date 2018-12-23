ec3391da8d499e951d01e42f59aeb3bd1266fb3a
Krzysztof Pniak <krzysztof.pniak@cardinality.co.uk>
2018-12-23 22:56:24 +0100

    style(nest): reformat with prettier

--- src/nest.js
+++ src/nest.js
@@ -1,34 +1,30 @@
 import {compose, lensProp, over, prop, set, startsWith, view} from 'ramda';
 
 const nest = (type, subReducer) => (state, action) => {
-    const prefix = `${type}.`;
-    const subState = view(lensProp(type), state);
-    const initializedState = subState
-        ? state
-        : set(
-              lensProp(type),
-              subReducer(undefined, {type: '@@NEST_INIT'}),
-              state
-          );
+  const prefix = `${type}.`;
+  const subState = view(lensProp(type), state);
+  const initializedState = subState
+    ? state
+    : set(lensProp(type), subReducer(undefined, {type: '@@NEST_INIT'}), state);
 
-    if (
-        compose(
-            startsWith(prefix),
-            prop('type')
-        )(action)
-    ) {
-        const unwrappedAction = {
-            ...action,
-            type: action.type.substr(prefix.length),
-        };
-        return over(
-            lensProp(type),
-            subState => subReducer(subState, unwrappedAction),
-            initializedState
-        );
-    } else {
-        return initializedState;
-    }
+  if (
+    compose(
+      startsWith(prefix),
+      prop('type')
+    )(action)
+  ) {
+    const unwrappedAction = {
+      ...action,
+      type: action.type.substr(prefix.length),
+    };
+    return over(
+      lensProp(type),
+      subState => subReducer(subState, unwrappedAction),
+      initializedState
+    );
+  } else {
+    return initializedState;
+  }
 };
 
 export default nest;
