315fbedf1700fcf9f3d437cb5d157f8d5d33fc38
Krzysztof Pniak <krzysztof.pniak@cardinality.co.uk>
2019-01-11 14:01:16 +0100

    refactor(actionType + actionType2): optimize type resolution

--- src/actionType.js
+++ src/actionType.js
@@ -1,9 +1,12 @@
 import {propEq, uncurryN} from 'ramda';
 import {getActionTypeFromCreatorOrString} from './helpers';
 
-const actionType = (type, transform) => (state, action) =>
-  propEq('type', getActionTypeFromCreatorOrString(type), action)
-    ? uncurryN(2, transform)(action.payload, state)
-    : state;
+const actionType = (type, transform) => {
+  const resolvedType = getActionTypeFromCreatorOrString(type);
+  return (state, action) =>
+    propEq('type', resolvedType, action)
+      ? uncurryN(2, transform)(action.payload, state)
+      : state;
+};
 
 export default actionType;
--- src/actionType2.js
+++ src/actionType2.js
@@ -1,9 +1,12 @@
 import {propEq, uncurryN} from 'ramda';
 import {getActionTypeFromCreatorOrString} from './helpers';
 
-const actionType2 = (type, transform) => (state, action) =>
-  propEq('type', getActionTypeFromCreatorOrString(type), action)
-    ? uncurryN(1, transform)(state)
-    : state;
+const actionType2 = (type, transform) => {
+  const resolvedType = getActionTypeFromCreatorOrString(type);
+  return (state, action) =>
+    propEq('type', resolvedType, action)
+      ? uncurryN(1, transform)(state)
+      : state;
+};
 
 export default actionType2;
